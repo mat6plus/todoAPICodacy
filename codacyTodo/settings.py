df50ffc26de3b5ece1c70a5a0f58c9e9f24cf2b0
Matthew Johnson <mat6plus@gmail.com>
2021-08-15 17:17:06 +0100

    Postgresql connect adjustments

--- codacyTodo/settings.py
+++ codacyTodo/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+# from dotenv import load_dotenv
+
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +80,32 @@ WSGI_APPLICATION = 'codacyTodo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': str(os.getenv('DB_NAME')),
+#             'USER': str(os.getenv('DB_USER')),
+#             'PASSWORD': (os.getenv('DB_USER_PASSWORD')),
+#             'HOST': (os.getenv('DB_HOST')),
+#             'PORT': (os.getenv('DB_PORT')),
+#         }
+# } 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'codacy',
+        'USER': 'matthew',
+        'PASSWORD': 'matman020',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
