5f5e575ff4db9907841c7a1b76b34dd14652cc61
Matthew <mat6plus@gmail.com>
2021-08-17 01:18:04 +0100

    URLs Update

--- todoapi/urls.py
+++ todoapi/urls.py
@@ -7,7 +7,7 @@ from .views import TodoList,TodoDetails,TodoCreate,TodoUpdate,TodoDelete
 urlpatterns = [
     path('', TodoList.as_view()),
     path('create', TodoCreate.as_view()),
-    path('<int:pk>/details', TodoDetails.as_view()),
-    path('<int:pk>/update', TodoUpdate.as_view()),
-    path('<int:pk>/delete', TodoDelete.as_view()),
+    path('detail/<int:pk>', TodoDetails.as_view()),
+    path('update/<int:pk>', TodoUpdate.as_view()),
+    path('delete/<int:pk>', TodoDelete.as_view()),
 ]
